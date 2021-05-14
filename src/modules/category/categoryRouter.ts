3284f47d702e4848764c669cbccd5cbe193051e5
Gass-Dev <madebygass@gmail.com>
2021-05-14 15:15:05 +0200

    getCategories + conception architecture category crud

--- src/modules/category/categoryRouter.ts
+++ src/modules/category/categoryRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { createCategoryController } from './useCases/createCategory/'
+import { getCategoriesController } from './useCases/getCategories'
 
 const categoryRouter: Router = Router();
 
 ///api/v1/skills/
 categoryRouter.post('/', (req, res) => createCategoryController.execute(req, res))
+categoryRouter.get('/', (req, res) => getCategoriesController.execute(req,res))
 
 export { categoryRouter }
---
+++ src/modules/category/useCases/getCategories/getCategoriesController.ts
@@ -0,0 +1,20 @@
+import { GetCategories } from './getCategories'
+import { Request, Response } from 'express'
+
+export class GetCategoriesController {
+    private useCase: GetCategories
+
+    constructor(getCategories: GetCategories) {
+        this.useCase = getCategories;
+    }
+
+    public async execute(_req: Request, res: Response) {
+
+        const categories = await this.useCase.execute()
+        console.log(categories);
+
+        res.status(200).json(categories)
+
+        res.status(200).json(categories)
+    }
+}
